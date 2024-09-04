9bd2eec962249a50cad1092c87fc377c609bd19f
wtrepair <westbrooktruckrepair@kisstech.ca>
2024-09-04 14:19:32 -0400

    first

---
+++ williamssolutionpro.vercel.appx/_next/static/-_iya7zGOXLPHoaQ2EfbM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-e3d1d557ef14fd01.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
